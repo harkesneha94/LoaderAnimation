7dc17de01caf65c817c736abf69e728092785cbf
Sneha Harke <Sneha.Harke@ril.com>
2019-08-20 18:20:42 +0530

    Initial Commit

---
+++ LoaderAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoaderAnimation
+//
+//  Created by Sneha Harke on 20/08/19.
+//  Copyright © 2019 Sneha Harke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
